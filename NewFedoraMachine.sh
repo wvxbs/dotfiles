225b08d0701e71db9c76a92ecfd4717d23f8385a
Gabriel Ferreira <gabriel.ferreira7854@gmail.com>
2022-03-18 09:39:41 -0300

    Update NewFedoraMachine.sh

--- NewFedoraMachine.sh
+++ NewFedoraMachine.sh
@@ -7,7 +7,14 @@ sudo dnf upgrade -y
 
 sudo dnf install -y firefox geary git neofetch npm python python3 python-pip marker gnome-pomodoro dnf-plugins-core flatpak neovim 
 
-sudo dnf install -y akmod-nvidia install xorg-x11-drv-nvidia-
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+sudo dnf update && sudo dnf install -y code
+
+sudo dnf install -y akmod-nvidia install xorg-x11-drv-nvidia-cuda
+
+curl https://cli-assets.heroku.com/install.sh | sh
 
 sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 
