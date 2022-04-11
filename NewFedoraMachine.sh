672d1524ef55cb1e1994eb26d8110707e9d413e3
wvxbs <gabriel.ferreira7854@gmail.com>
2022-04-11 09:46:01 -0300

    NewFedoraMachine updated

--- NewFedoraMachine.sh
+++ NewFedoraMachine.sh
@@ -5,18 +5,14 @@ sudo dnf update -y
 
 sudo dnf upgrade -y
 
-sudo dnf install -y firefox geary git neofetch npm python python3 python-pip marker gnome-pomodoro dnf-plugins-core flatpak neovim 
+sudo dnf install -y firefox geary git neofetch npm python python3 python-pip marker gnome-pomodoro dnf-plugins-core flatpak neovim zsh
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 sudo dnf update && sudo dnf install -y code
 
-sudo dnf install -y akmod-nvidia install xorg-x11-drv-nvidia-cuda
-
 curl https://cli-assets.heroku.com/install.sh | sh
-
-sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
@@ -71,3 +67,6 @@ flatpak install flathub io.github.lainsce.Emulsion
 git config --global user.name $USER_NAME
 
 git config --global user.email $EMAIL
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
