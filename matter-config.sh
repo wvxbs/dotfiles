ac99c768d23839555a5f87e4d36c2cb9250eb53a
wvxbs <gabriel.ferreira7854@gmail.com>
2022-09-25 14:40:01 -0300

    aliases and matter-config updated

--- matter-config.sh
+++ matter-config.sh
@@ -2,4 +2,5 @@ git clone https://github.com/mateosss/matter
 
 cd matter
 
+./matter.py -i microsoft-windows-classic cog -bg 282C34 -fg 939599 -hl 535559dd
 ./matter.py -i fedora fedora fedora microsoft-windows-classic cog -bg 282C34 -fg 939599 -hl 535559dd
